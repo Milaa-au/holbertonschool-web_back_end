318644502efbf799526a59ba25faa1c359746a6f
Milaa-au <mila.audu@icloud.coom>
2026-05-15 12:50:00 +0000

    correction

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,23 +1,60 @@
+const fs = require('fs');
 const express = require('express');
-const countStudents = require('./3-read_file_async');
 
 const app = express();
+const port = 1245;
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = data.split('\n').slice(1).filter((line) => line.trim() !== '');
+      const fields = {};
+
+      for (const line of lines) {
+        const parts = line.split(',');
+        const firstname = parts[0];
+        const field = parts[parts.length - 1];
+
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(firstname);
+      }
+
+      let text = `Number of students: ${lines.length}`;
+      for (const field in fields) {
+        if (Object.prototype.hasOwnProperty.call(fields, field)) {
+          text += `\nNumber of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`;
+        }
+      }
+
+      resolve(text);
+    });
+  });
+}
 
 app.get('/', (req, res) => {
-  res.type('text/plain');
+  res.type('text');
   res.send('Hello Holberton School!');
-})
+});
 
 app.get('/students', (req, res) => {
-  res.type('text/plain');
+  res.type('text');
+
   countStudents(process.argv[2])
-    .then((output) => {
-      res.send(`This is the list of our students\n${output}`);
+    .then((text) => {
+      res.send(`This is the list of our students\n${text}`);
     })
     .catch((err) => {
-      res.send(`${err}`);
+      res.send(`This is the list of our students\n${err.message}`);
     });
 });
 
-app.listen(1245);
+app.listen(port);
+
 module.exports = app;
